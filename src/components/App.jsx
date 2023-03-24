3bdc63c67cd6d7e7655356b1dbe09d02dc04c558
OlehBrov <o.dudnyk85@gmail.com>
2023-03-24 11:59:14 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,56 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Container } from './container/container.styled';
+import { Component } from 'react';
+import { Statistics } from './feedbackControls/Statistics';
+import { FeedbackOptions } from './feedbackControls/FeedbackOptions/FeedbackOptions';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onFeedbackClick = ({ target }) => {
+    this.setState(prevState => {
+      return { [target.name]: prevState[target.name] + 1 };
+    });
+  };
+
+  totalFeedbacks = state => {
+    return Object.values(state).reduce((previousValue, number) => {
+      return previousValue + number;
+    }, 0);
+  };
+  positivePercentage = state => {
+    if (this.totalFeedbacks(this.state) !== 0) {
+      return (
+        Math.round((state.good / this.totalFeedbacks(this.state)) * 100) + '%'
+      );
+    }
+    return 0;
+  };
+
+  render() {
+    return (
+      <Container>
+        <div title="Please, leave a feedback">
+          <h1>Please, leave a feedback</h1>
+          <FeedbackOptions
+            options={this.state}
+            onLeaveFeedback={this.onFeedbackClick}
+          ></FeedbackOptions>
+        </div>
+        <div title="Statistics">  
+          <h2>Statistisc</h2>
+          <Statistics
+            good={this.state.good}
+            neutral={this.state.neutral}
+            bad={this.state.bad}
+            total={this.totalFeedbacks(this.state)}
+            positivePercentage={this.positivePercentage(this.state)}
+          ></Statistics>
+        </div>
+      </Container>
+    );
+  }
+}
